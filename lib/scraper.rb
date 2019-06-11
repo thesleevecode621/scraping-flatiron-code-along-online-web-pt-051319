c4ab7065b3de5cff524ea16495b2a8692628a2a6
boruch d <dubywider@gmail.com>
2019-06-11 20:33:28 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -16,8 +16,7 @@ class Scraper
      # course.schedule = post.css(".date").text
      # course.description = post.css("p").text
       
-    end
-  end
+   
   binding.pry
     def print_courses
     self.make_courses
